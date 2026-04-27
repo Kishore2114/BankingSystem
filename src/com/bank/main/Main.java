3bc4870f358f5a28d27396b707c5c28253f70128
kishore <kishor9102309@email.com>
2026-04-27 12:40:26 +0530

    c

--- src/com/bank/main/Main.java
+++ src/com/bank/main/Main.java
@@ -2,7 +2,7 @@ package com.bank.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Bank Application!");
+        System.out.println("Welcome ");
         // You can add code here to initialize your application, such as creating accounts, etc.
     }
 }
